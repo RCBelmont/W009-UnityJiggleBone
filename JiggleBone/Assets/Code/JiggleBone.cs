abf40567778d85806ee8ab2f3d695f5869fc60ef
RCBelmont <dbelmont@foxmail.com>
2019-11-10 00:04:14 +0800

    更新代码
    更新代码

--- JiggleBone/Assets/Code/JiggleBone.cs
+++ JiggleBone/Assets/Code/JiggleBone.cs
@@ -15,7 +15,7 @@ class BoneParticle
     public int BoneIdx = 0;
     public float DefaultDisToParent = 0.0f;
     public Quaternion DefaultRotate;
-
+    public Vector3 Velocity = Vector3.zero;
     public BoneParticle(GameObject obj, bool isRoot, int boneIdx = 0, BoneParticle parentBone = null)
     {
         GO = obj;
@@ -25,6 +25,7 @@ class BoneParticle
         DefaultLocalPos = BoneTransform.localPosition;
         BoneIdx = boneIdx;
         ParentBone = parentBone;
+        
         if (parentBone != null)
         {
             DefaultDisToParent = Vector3.Distance(BoneTransform.position, parentBone.BoneTransform.position);
@@ -72,17 +73,101 @@ public class JiggleBone : MonoBehaviour
                 continue;
             }
 
-            Vector3 moveDir = bp.TargetPos - bp.CurrPos;
-            float totalDistance = moveDir.magnitude;
+            Vector3 X = bp.BoneTransform.right;
+            Vector3 forceDir = bp.TargetPos - bp.CurrPos;
+            float distanceToOPos = forceDir.magnitude;
+            
+            forceDir = forceDir.normalized;
+            float sprint = 50.0f;
+            float forceStr = sprint / bp.BoneIdx * distanceToOPos; 
+            float mass = 3.0f;
+            
+            Vector3 parentToThisDir = (bp.CurrPos - bp.ParentBone.CurrPos).normalized;
+            //parentToThisDir = (bp.CurrPos - bp.ParentBone.CurrPos).normalized;
+            //bp.CurrPos = bp.ParentBone.CurrPos + parentToThisDir * bp.DefaultDisToParent;
+            forceDir = Vector3.Cross(parentToThisDir, forceDir).normalized;
+            forceDir = Vector3.Cross(forceDir, parentToThisDir).normalized;
+            Vector3 accelerate = (forceStr / mass) * forceDir;
 
-            Vector3 dynamicPos = bp.CurrPos + moveDir.normalized * (totalDistance / (Stiff * bp.BoneIdx));
-            Vector3 parentToThisDir = (dynamicPos - bp.ParentBone.CurrPos).normalized;
+            bp.Velocity += accelerate * Time.deltaTime;
+            bp.Velocity = Vector3.Dot(bp.Velocity, forceDir) * 0.99f * forceDir;
+            
+            Vector3 dynamicPos = bp.CurrPos + bp.Velocity * Time.deltaTime;
+            parentToThisDir = (dynamicPos - bp.ParentBone.CurrPos).normalized;
             bp.CurrPos = bp.ParentBone.CurrPos + parentToThisDir * bp.DefaultDisToParent;
             
-           
+         
             bp.BoneTransform.position = bp.CurrPos; //bp.CurrPos;
-            bp.BoneTransform.LookAt(bp.ParentBone.CurrPos);
+            //bp.BoneTransform.LookAt(bp.ParentBone.CurrPos, _rootBone.BoneTransform.forward);
+            Vector3 lookDir = (bp.CurrPos - bp.ParentBone.CurrPos).normalized;
+            //lookDir = Vector3.Cross(bp.BoneTransform.right, lookDir);
+            
+            bp.BoneTransform.rotation = QuaternionLookRotation(lookDir, X);
         }
+    }
+
+    private Quaternion QuaternionLookRotation(Vector3 forward, Vector3 up)
+    {
+        forward.Normalize();
+
+        Vector3 vector = Vector3.Normalize(forward);
+        Vector3 vector2 = Vector3.Normalize(Vector3.Cross(up, vector));
+        Vector3 vector3 = Vector3.Cross(vector, vector2);
+        var m00 = vector3.x;
+        var m01 = vector3.y;
+        var m02 = vector3.z;
+        
+        var m10 = vector.x;
+        var m11 = vector.y;
+        var m12 = vector.z;
+        
+        var m20 = vector2.x;
+        var m21 = vector2.y;
+        var m22 = vector2.z;
+
+
+        float num8 = (m00 + m11) + m22;
+        var quaternion = new Quaternion();
+        if (num8 > 0f)
+        {
+            var num = (float) Math.Sqrt(num8 + 1f);
+            quaternion.w = num * 0.5f;
+            num = 0.5f / num;
+            quaternion.x = (m12 - m21) * num;
+            quaternion.y = (m20 - m02) * num;
+            quaternion.z = (m01 - m10) * num;
+            return quaternion;
+        }
+
+        if ((m00 >= m11) && (m00 >= m22))
+        {
+            var num7 = (float) Math.Sqrt(((1f + m00) - m11) - m22);
+            var num4 = 0.5f / num7;
+            quaternion.x = 0.5f * num7;
+            quaternion.y = (m01 + m10) * num4;
+            quaternion.z = (m02 + m20) * num4;
+            quaternion.w = (m12 - m21) * num4;
+            return quaternion;
+        }
+
+        if (m11 > m22)
+        {
+            var num6 = (float) Math.Sqrt(((1f + m11) - m00) - m22);
+            var num3 = 0.5f / num6;
+            quaternion.x = (m10 + m01) * num3;
+            quaternion.y = 0.5f * num6;
+            quaternion.z = (m21 + m12) * num3;
+            quaternion.w = (m20 - m02) * num3;
+            return quaternion;
+        }
+
+        var num5 = (float) Math.Sqrt(((1f + m22) - m00) - m11);
+        var num2 = 0.5f / num5;
+        quaternion.x = (m20 + m02) * num2;
+        quaternion.y = (m21 + m12) * num2;
+        quaternion.z = 0.5f * num5;
+        quaternion.w = (m01 - m10) * num2;
+        return quaternion;
     }
 
     private void SetBoneToDefaultPos()
@@ -92,6 +177,7 @@ public class JiggleBone : MonoBehaviour
             BoneParticle bp = _boneList[i];
             if (bp.IsRoot)
             {
+                bp.TargetPos = bp.BoneTransform.position;
                 continue;
             }
 
@@ -111,6 +197,15 @@ public class JiggleBone : MonoBehaviour
             BoneParticle newParticle = new BoneParticle(child.gameObject, false, _boneList.Count, root);
             _boneList.Add(newParticle);
             CrateChildBoneParticle(newParticle);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        foreach (var boneParticle in _boneList)
+        {
+            Gizmos.DrawWireSphere(boneParticle.TargetPos,  1.5f);
+            Gizmos.DrawLine(boneParticle.BoneTransform.position, boneParticle.BoneTransform.position +boneParticle.Velocity);
         }
     }
 }
